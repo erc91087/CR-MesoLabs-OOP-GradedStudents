2a2bbf89b37a5cfc6c631c96538e8ef93db6048e
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-02-18 20:38:47 -0500

    Still working thru part 1

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,91 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+
+
+    // instance variables
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+
+    // constructor, initializing variables
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList(Arrays.asList(testScores));
+        // The asList() method returns a list view of the specified array.
+
+
+    }
+
+
+    // generated getters & setters
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+
+    public String getExamScores() { //define method
+
+        StringBuilder listOfScores = new StringBuilder();
+
+        listOfScores.append("Exam Scores: " + "\n");
+        for(int i = 0; i < examScores.size(); i++){
+            listOfScores.append("Exam " + (i + 1)  + " -> " + examScores.get(i) + "\n");
+        }
+        System.out.println(listOfScores.toString());
+        return listOfScores.toString();
+    }
+
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+
+    public void addExamScore(double examScore) {
+        examScores.add(examScore);
+    }
+
+
+    public void setExamScore(int examNumber, double newScore) {
+        this.examScores.set(examNumber - 1, newScore);
+    }
+
+
+
+    public double getAverageExamScore() {
+
+        double totalExams = 0;
+
+        for (Double scores : this.examScores) {
+            totalExams += scores;
+            }
+
+        return Math.round(totalExams / this.examScores.size());
+
+        // this.examScores = new ArrayList(Arrays.asList(testScores));
+        // method didn't work w/o Arrays.asList() ^^^^^
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,146 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class StudentTest {
+
+
+    private Student testStudent;
+
+//    @Before
+//    public void setUp() {
+//        // : Given
+//        String firstName = "Eric";
+//        String lastName = "Cordell";
+//
+//        Double[] examScores = {100.0, 95.0, 123.0, 98.0};
+//        testStudent = new Student(firstName, lastName, examScores);
+//    }
+
+    @Test
+    public void setFirstNameTest() {
+        //  Given
+        String firstName = "Eric";
+        String lastName = "Cordell";
+
+        Double[] examScores = {100.0, 95.0, 123.0, 98.0};
+        testStudent = new Student(firstName, lastName, examScores);
+        String expected = "Eric";
+
+        //  When
+        testStudent.setFirstName(expected);
+        String actual = testStudent.getFirstName();
+
+        //  Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setLastNameTest() {
+        //  Given
+        String firstName = "Eric";
+        String lastName = "Cordell";
+
+        Double[] examScores = {100.0, 95.0, 123.0, 98.0};
+        testStudent = new Student(firstName, lastName, examScores);
+        String expected = "Cordell";
+
+        //  When
+        testStudent.setLastName(expected);
+        String actual = testStudent.getLastName();
+
+        //  Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getExamScoresTest() {
+        // : Given
+        String firstName = "Eric";
+        String lastName = "Cordell";
+        Double[] examScores = {100.0, 95.0, 123.0, 96.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void getNumberOfExamsTaken() {
+        //  Given
+        String firstName = "Eric";
+        String lastName = "Cordell";
+
+        Double[] examScores = {100.0, 95.0, 123.0, 98.0};
+        testStudent = new Student(firstName, lastName, examScores);
+        int expected = 4;
+
+        //  When
+        int actual = testStudent.getNumberOfExamsTaken();
+
+        //  Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void addExamScoreTest() {
+        // : Given
+        String firstName = "Eric";
+        String lastName = "Cordell";
+        Double[] examScores = {};
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.addExamScore(100.0);
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void setExamScoreTest() {
+        // : Given
+
+        String firstName = "Eric";
+        String lastName = "Cordell";
+        Double[] examScores = { 40.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.setExamScore(1, 150.0);
+        String actual = testStudent.getExamScores();
+        String expected = "Exam 1 -> 150.0";
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAverageExamScoreTest() {
+        // : Given
+        String firstName = "Eric";
+        String lastName = "Cordell";
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.00 };
+        Student student = new Student(firstName, lastName, examScores);
+        double expected = 125;
+
+        // When
+        double actual = student.getAverageExamScore();
+
+
+        // Then
+        Assert.assertEquals(expected, actual, .001);
+    }
+
+
 
 }
